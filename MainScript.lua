b7cb0dccff13a8ea24c39a2b59f0091cf9110799
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-08-01 23:12:57 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,11 +1,12 @@
---[[local Config = {
+if Config == nil then
+local Config = {
     Headless = true,
     Korblox = true, -- ONLY R15
 
     FakeName = "Protected",
     FakeId = 1, -- input id
 }
-]]
+end
 
 oldUserId = game.Players.LocalPlayer.UserId
 oldName = game.Players.LocalPlayer.Name

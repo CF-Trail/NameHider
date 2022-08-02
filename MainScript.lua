96d15aa198d61f3bd513dd372f83078b43e837b6
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-08-02 15:52:50 +0300

    full credits going to github.com/ImMejor35 (Tomato)

--- MainScript.lua
+++ MainScript.lua
@@ -1,64 +1,58 @@
 if not getgenv().Config then
     getgenv().Config = {
     Headless = true,
-
-    FakeName = "Protected",
+    
+    FakeDisplayName = "FakeDisplayName ",
+    FakeName = "FakeName",
     FakeId = 1, -- input id
 }
 end
-oldUserId = game:GetService("Players").LocalPlayer.UserId
-oldName = game:GetService("Players").LocalPlayer.Name
-oldDisplayName = game:GetService("Players").LocalPlayer.DisplayName
-
+lp = game:GetService("Players").LocalPlayer
+oldUserId = tostring(lp.UserId)
+oldName = lp.Name
+oldDisplayName = lp.DisplayName
+local function processtext(text)
+    if string.gsub(text,oldName,Config.FakeName) ~= text then
+        return string.gsub(text,oldName,Config.FakeName)
+    elseif string.gsub(text,oldUserId,Config.FakeId) ~= text then
+        return string.gsub(text,oldUserId,Config.FakeId)
+    elseif string.gsub(text,oldDisplayName,Config.FakeDisplayName) ~= text then
+        return string.gsub(text,oldDisplayName,Config.FakeDisplayName)
+    end
+    if text ~= nil then
+        return text
+    end
+    return ''
+end
 for i,v in next, game:GetDescendants() do
-if v:IsA("TextBox") or v:IsA("TextLabel") or v:IsA("TextButton") then
-if string.find(v.Text,oldName) or string.find(v.Text,oldDisplayName) or string.find(v.Text,oldUserId) then
-v.Text = getgenv().Config.FakeName
-end
-elseif v.Name == oldName or v.Name == oldDisplayName or v.Name == oldUserId then
-    v.Name = getgenv().Config.FakeName
-end
+    if v:IsA("TextBox") or v:IsA("TextLabel") or v:IsA("TextButton") then
+        v.Text = processtext(v.Text)
+        v.Name = processtext(v.Name)
+        v.Changed:Connect(function(property)
+            v.Text = processtext(v.Text)
+            v.Name = processtext(v.Name)
+        end)
+    end
 end
 game.DescendantAdded:Connect(function(descendant)
     if descendant:IsA("TextBox") or descendant:IsA("TextLabel") or descendant:IsA("TextButton") then
+        descendant.Text = processtext(descendant.Text)
+        descendant.Name = processtext(descendant.Name)
         descendant.Changed:Connect(function()
-            if string.find(descendant.Text,oldName) or string.find(descendant.Text,oldDisplayName) or string.find(descendant.Text,oldUserId) then
-                descendant.Text = getgenv().Config.FakeName
-            end
-         end)
-        if string.find(descendant.Text,oldName) or string.find(descendant.Text,oldDisplayName) or string.find(descendant.Text,oldUserId) then
-            descendant.Text = getgenv().Config.FakeName
-        end
-     elseif descendant.Name == oldName or descendant.Name == oldDisplayName or descendant.Name == oldUserId then
-        descendant.Name = getgenv().Config.FakeName
-     end
+            descendant.Text = processtext(descendant.Text)
+            descendant.Name = processtext(descendant.Name)
+        end)
+    end
 end)
+lp.DisplayName = Config.FakeDisplayName
+lp.CharacterAppearanceId = Config.FakeId
 
-for i,v in next, game:GetDescendants() do
-    if v:IsA("TextBox") or v:IsA("TextLabel") or v:IsA("TextButton") then
-       v.Changed:Connect(function(property)
-            if string.find(v.Text,oldName) or string.find(v.Text,oldDisplayName) or string.find(v.Text,oldUserId) then
-                v.Text = getgenv().Config.FakeName
-            end
-       end)
+if Config.Headless == true then
+   while wait() do
+        local char = lp.Character or lp.CharacterAdded:wait()
+        char:WaitForChild("Head").Transparency = 1
+        if char:WaitForChild("Head"):FindFirstChildOfClass("Decal") then
+            char.Head:FindFirstChildOfClass("Decal"):Destroy()
+        end
     end
-end
---[[
-game.Players.LocalPlayer.Name = getgenv().FakeName
-]]
-game:GetService("Players").LocalPlayer.DisplayName = getgenv().Config.FakeName
-game:GetService("Players").LocalPlayer.CharacterAppearanceId = getgenv().Config.FakeId
---[[
-game.Players.LocalPlayer.Character.Name = getgenv().FakeName
-game.Players.LocalPlayer.UserId = getgenv().UserId
-]]
-
-if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Head") and getgenv().Config.Headless == true then
-    while wait(2) do
-    workspace:WaitForChild(game:GetService("Players").LocalPlayer.Name):WaitForChild("Head")
-    game:GetService("Players").LocalPlayer.Character.Head.Transparency = 1
-    if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Head"):FindFirstChildOfClass("Decal") then
-    game:GetService("Players").LocalPlayer.Character.Head:FindFirstChildOfClass("Decal"):Destroy()
-    end
-end
 end

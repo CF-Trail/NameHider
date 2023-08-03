4cba4ad92ec0adf93448dd6001342dadceecf662
szze <106690765+CF-Trail@users.noreply.github.com>
2023-08-04 00:14:14 +0300

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,12 +1,92 @@
 if not getgenv().Config then
     getgenv().Config = {
-    Headless = true,
+    Headless = false,
     
-    FakeDisplayName = "FakeDisplayName ",
-    FakeName = "FakeName",
-    FakeId = 1, -- input id
+    FakeDisplayName = "AmySchumer",
+    FakeName = "redmiint8",
+    FakeId = 13886182,
 }
 end
+
+local players = game:GetService('Players')
+
+function disguisechar(char, id)
+	task.spawn(function()
+        print('called')
+		if not char then
+			return
+		end
+		local hum = char:FindFirstChildOfClass('Humanoid')
+		char:WaitForChild("Head")
+		local desc
+		if desc == nil then
+			local suc = false
+			repeat
+				suc = pcall(function()
+					desc = players:GetHumanoidDescriptionFromUserId(id)
+				end)
+				task.wait(1)
+			until suc
+		end
+		desc.HeightScale = hum:WaitForChild("HumanoidDescription").HeightScale
+		char.Archivable = true
+		local disguiseclone = char:Clone()
+		disguiseclone.Name = "disguisechar"
+		disguiseclone.Parent = workspace
+		for i, v in pairs(disguiseclone:GetChildren()) do
+			if v:IsA("Accessory") or v:IsA("ShirtGraphic") or v:IsA("Shirt") or v:IsA("Pants") then
+				v:Destroy()
+			end
+		end
+		disguiseclone.Humanoid:ApplyDescriptionClientServer(desc)
+		for i, v in pairs(char:GetChildren()) do
+			if (v:IsA("Accessory") and v:GetAttribute("InvItem") == nil and v:GetAttribute("ArmorSlot") == nil) or v:IsA("ShirtGraphic") or v:IsA("Shirt") or v:IsA("Pants") or v:IsA("BodyColors") then
+				v.Parent = game
+			end
+		end
+		char.ChildAdded:Connect(function(v)
+			if ((v:IsA("Accessory") and v:GetAttribute("InvItem") == nil and v:GetAttribute("ArmorSlot") == nil) or v:IsA("ShirtGraphic") or v:IsA("Shirt") or v:IsA("Pants") or v:IsA("BodyColors")) and v:GetAttribute("Disguise") == nil then
+				repeat
+					task.wait()
+					v.Parent = game
+				until v.Parent == game
+			end
+		end)
+		for i, v in pairs(disguiseclone:WaitForChild("Animate"):GetChildren()) do
+			v:SetAttribute("Disguise", true)
+			local real = char.Animate:FindFirstChild(v.Name)
+			if v:IsA("StringValue") and real then
+				real.Parent = game
+				v.Parent = char.Animate
+			end
+		end
+		for i, v in pairs(disguiseclone:GetChildren()) do
+			v:SetAttribute("Disguise", true)
+			if v:IsA("Accessory") then
+				for i2, v2 in pairs(v:GetDescendants()) do
+					if v2:IsA("Weld") and v2.Part1 then
+						v2.Part1 = char[v2.Part1.Name]
+					end
+				end
+				v.Parent = char
+			elseif v:IsA("ShirtGraphic") or v:IsA("Shirt") or v:IsA("Pants") or v:IsA("BodyColors") then
+				v.Parent = char
+			elseif v.Name == "Head" and v:FindFirstChildOfClass('SpecialMesh') then
+				char.Head:FindFirstChildOfClass('SpecialMesh').MeshId = v:FindFirstChildOfClass('SpecialMesh').MeshId
+			end
+		end
+		local localface = char:FindFirstChild("face", true)
+		local cloneface = disguiseclone:FindFirstChild("face", true)
+		if localface and cloneface then
+			localface.Parent = game
+			cloneface.Parent = char.Head
+		end
+		char.Humanoid.HumanoidDescription:SetEmotes(desc:GetEmotes())
+		char.Humanoid.HumanoidDescription:SetEquippedEmotes(desc:GetEquippedEmotes())
+		disguiseclone:Destroy()
+	end)
+end
+
 lp = game:GetService("Players").LocalPlayer
 oldUserId = tostring(lp.UserId)
 oldName = lp.Name
@@ -48,6 +128,7 @@ lp.DisplayName = Config.FakeDisplayName
 lp.CharacterAppearanceId = Config.FakeId
 
 if Config.Headless == true then
+    task.spawn(function()
    while wait() do
         local char = lp.Character or lp.CharacterAdded:wait()
         char:WaitForChild("Head").Transparency = 1
@@ -55,4 +136,13 @@ if Config.Headless == true then
             char.Head:FindFirstChildOfClass("Decal"):Destroy()
         end
     end
+    end)
 end
+
+pcall(function()
+   disguisechar(lp.Character,getgenv().Config.FakeId)
+end)
+
+lp.CharacterAdded:Connect(function()
+      disguisechar(lp.Character,getgenv().Config.FakeId)
+end)
